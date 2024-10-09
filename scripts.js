b3de0432ebf917f1e53610d37ed9c7ed440e49d9
RyanVNgo <vanryan711@gmail.com>
2024-10-08 21:56:21 -0700

    Fixed invisible links bug and restructured code/files

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('get-in-touch').addEventListener('click', function() {
-  const images = document.getElementById('social-links');
-  if (images.style.opacity == 0) {
-    images.style.opacity = 1;
-  } else {
-    images.style.opacity = 0;
-  }
-});
