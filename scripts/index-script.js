261908bb7e52adb1ae8479a067cd452f78ce049d
RyanVNgo <vanryan711@gmail.com>
2024-10-22 01:29:04 -0700

    Shift to cleaner redesign

--- scripts/index-script.js
+++
@@ -1,9 +0,0 @@
-  document.querySelectorAll('.card').forEach(card => {
-      card.addEventListener('click', function() {
-          const link = this.getAttribute('data-link');
-          if (link) {
-              window.open(link, '_blank') = link; 
-          }
-      });
-  });
-
